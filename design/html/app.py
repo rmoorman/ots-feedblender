e8f7d2bba5a1ee07c3b8eea2192cfb5624e7caa8
Rico Moorman <rico.moorman@gmail.com>
2015-01-30 23:34:09 +0100

    Add delete screen, add form markup and basic (flash) message styling

--- design/html/app.py
+++ design/html/app.py
@@ -1,15 +1,20 @@
 from flask import Flask
-from flask import url_for, render_template, Response
+from flask import url_for, render_template, flash, Response
 
 
 app = Flask(__name__)
+app.secret_key = "A962B60B-765C-49A1-AF45-7C061605B34F"
 
 
 @app.route("/")
 def index():
     pages = [
         url_for("add"),
+        url_for("added"),
         url_for("edit"),
+        url_for("edited"),
+        url_for("delete"),
+        url_for("deleted"),
         url_for("list"),
     ]
     return render_template("index.html", pages=pages)
@@ -18,9 +23,28 @@ def index():
 def add():
     return render_template("add.html")
 
+@app.route("/list/added/")
+def added():
+    flash("The new blend has been added.")
+    return render_template("list.html", active_endpoint="list")
+
 @app.route("/edit/")
 def edit():
     return render_template("edit.html")
+
+@app.route("/list/edited/")
+def edited():
+    flash("The blend data has been updated successfully.")
+    return render_template("list.html", active_endpoint="list")
+
+@app.route("/delete/")
+def delete():
+    return render_template("delete.html")
+
+@app.route("/list/deleted/")
+def deleted():
+    flash("The blend has been deleted.")
+    return render_template("list.html", active_endpoint="list")
 
 @app.route("/list/")
 def list():
